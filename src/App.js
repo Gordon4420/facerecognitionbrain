72bd5b17febc1dd56f1406b5e4e84c07392d5b74
Gordon4420 <gordon@gordonmclean.me>
2018-08-22 02:45:21 +0200

    background fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 const particleOptions = {
   particles: {
     number: {
-      value: 80,
+      value: 300,
       density: {
         enable: true,
         value_area: 800,
